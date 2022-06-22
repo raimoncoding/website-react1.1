8392ca883c7797c7d58af55afa9dbf40ad321eb3
raimoncoding <robertjanstefan@gmail.com>
2022-06-22 13:45:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import { HashRouter } from "react-router-dom";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
